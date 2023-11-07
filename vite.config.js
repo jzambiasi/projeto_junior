b1a9ca5a5bb404831e4834e6a0231c77ee8199f3
jzambiasi <112366087+jzambiasi@users.noreply.github.com>
2023-11-07 20:13:47 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 export default{
     root: 'src',
-    base:'https://github.com/jzambiasi/projeto_junior/',
+    base:'https://jzambiasi.github.io/projeto_junior/',
     build:{
         outDir:'../dist'
     }
